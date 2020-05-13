426d5bb7cc82b59a03e373374ffb23a2f1d3429b
shikha10799 <52153512+shikha10799@users.noreply.github.com>
2020-05-13 16:40:07 +0530

    Update application.py

--- application.py
+++ application.py
@@ -12,8 +12,8 @@ from decorators import login_required
 app = Flask(__name__, static_url_path='/static')
 
 #Check for environment variable
-if not os.getenv("DATABASE_URL"):
-   raise RuntimeError("DATABASE_URL is not set")
+#if not os.getenv("DATABASE_URL"):
+   #raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -21,7 +21,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(process.env.DATABASE_URL)
 db = scoped_session(sessionmaker(bind=engine))
 
 
